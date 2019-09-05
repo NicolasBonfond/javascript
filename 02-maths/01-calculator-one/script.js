a08acd62fde08d110308358d76155f2c7f215f40
becode <nicolas.bonfond@outlook.com>
2019-09-05 10:44:54 +0200

    2.1 done

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -13,18 +13,34 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", () => {
+        var op1 = document.getElementById("op-one").value;
+        var op2 = document.getElementById("op-two").value;
+        var aResult = parseInt(op1) + parseInt(op2);
+        alert (aResult);
         // perform an addition
     });
 
     document.getElementById("substraction").addEventListener("click", () => {
+        var op1 = document.getElementById("op-one").value;
+        var op2 = document.getElementById("op-two").value;
+        var sResult = op1 - op2;
+        alert (sResult);
         // perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", () => {
+        var op1 = document.getElementById("op-one").value;
+        var op2 = document.getElementById("op-two").value;
+        var mResult = op1 * op2;
+        alert (mResult);
         // perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", () => {
+        var op1 = document.getElementById("op-one").value;
+        var op2 = document.getElementById("op-two").value;
+        var mResult = op1 / op2;
+        alert (mResult);
         // perform an division
     });
 })();
